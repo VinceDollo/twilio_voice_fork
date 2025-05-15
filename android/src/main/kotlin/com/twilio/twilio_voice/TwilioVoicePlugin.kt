2225c22cd9de579f881ef886bc6e5c055bac2ab4
Vincent DOLLO <vincent@mobizel.com>
2025-05-15 14:44:09 +0200

    (fix: crash_release_incoming_call) add debuggable into manifest

--- android/src/main/kotlin/com/twilio/twilio_voice/TwilioVoicePlugin.kt
+++ android/src/main/kotlin/com/twilio/twilio_voice/TwilioVoicePlugin.kt
@@ -1642,8 +1642,6 @@ class TwilioVoicePlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamH
             }
 
             TVBroadcastReceiver.ACTION_INCOMING_CALL -> {
-
-
                 // TODO
                 val callHandle =
                     intent.getStringExtra(TVBroadcastReceiver.EXTRA_CALL_HANDLE) ?: run {
